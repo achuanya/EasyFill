47cc05d122d1e72cb484b70a099c5c796b3745e0
achuanya <haibao1027@gmail.com>
2025-03-27 21:26:37 +0800

    update wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -21,11 +21,11 @@ export default defineConfig({
     action: {
       default_icon: {
         '16': 'icon/16.png',
-        '32': 'icon/48.png',
+        '32': 'icon/32.png',
         '48': 'icon/48.png',
         '128': 'icon/128.png'
       },
-      default_title: '简易填充'
+      default_title: 'EasyFill'
     },
     background: {
       service_worker: 'background.js'
