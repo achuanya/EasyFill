a3e19b66225d6b856a6bde75f3fa355bd4e9ff37
achuanya <haibao1027@gmail.com>
2025-03-27 21:18:04 +0800

    update wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     description: '简易填充，让每一次评论更自然，与你的博友互动无缝连接',
     icons: {
       '16': 'icon/16.png',
-      '32': 'icon/48.png',
+      '32': 'icon/32.png',
       '48': 'icon/48.png',
       '128': 'icon/128.png'
     },
